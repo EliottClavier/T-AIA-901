013f7d235d25d79cb30e5136888100c45eb67048
Clavier Eliott <eliott.clavier@epitech.eu>
2023-10-11 19:34:17 +0200

    [AIA-STYLE] Refactoring ItineraryPage

--- mobile/lib/screens/ItineraryPage.dart
+++ mobile/lib/screens/ItineraryPage.dart
@@ -22,7 +22,7 @@ class ItineraryPage extends StatelessWidget {
     }
   }
 
-  Widget _createEmptyContainer() {
+  Container _createEmptyContainer() {
     return Container(
         decoration: BoxDecoration(
           // No border
@@ -34,137 +34,153 @@ class ItineraryPage extends StatelessWidget {
     );
   }
 
+  List<Widget> _getListHeader(BuildContext context) {
+    return [
+      AppBar(
+          backgroundColor: AppColors.backgroundColor,
+          elevation: 0,
+          title: CustomText(
+            text: "Retour",
+            fontSize: 22.0,
+            fontWeight: FontWeight.w600,
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            iconSize: 40.0,
+            onPressed: () {
+              NavigationService.backToVoiceRecognitionPage();
+            },
+          )
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Divider(
+              color: AppColors.whiteColor,
+              thickness: 1,
+              height: 40,
+            ),
+            ItineraryComponent(
+              prependText: "Départ : ",
+              text: " ${itineraryResponse.departure}",
+            ),
+            SizedBox(height: 15.0),
+            ItineraryComponent(
+              prependText: "Arrivée : ",
+              text: " ${itineraryResponse.destination}",
+            ),
+            Divider(
+              color: AppColors.whiteColor,
+              thickness: 1,
+              height: 40,
+            ),
+            ItineraryComponent(
+              prependText: "Durée totale : ",
+              text: "_H__",
+            ),
+            SizedBox(height: 30.0),
+            CustomText(
+              text: "Résume de votre voyage",
+              fontSize: 22.0,
+            ),
+            SizedBox(height: 30.0)
+          ],
+        ),
+      )
+    ];
+  }
+
+  Row _getDotRow(BuildContext context, int index) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 2,
+          child: ItineraryComponentDot(
+            type: index == 0
+                ? ItineraryComponentDotType.departure
+                : index == itineraryResponse.steps.length + 1
+                ? ItineraryComponentDotType.destination
+                : ItineraryComponentDotType.step,
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: _createEmptyContainer(),
+        ),
+        Expanded(
+          flex: 14,
+          child: ItineraryComponent(
+            text: "${_getCurrentStep(index)}",
+          ),
+        )
+      ],
+    );
+  }
+
+  Row _getLineRow(BuildContext context, int index) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: index == itineraryResponse.steps.length + 1 ?
+          _createEmptyContainer() :
+          Container(
+            height: MediaQuery.of(context).size.height * 0.15,
+            decoration: BoxDecoration(
+              color: AppColors.whiteColor,
+              border: Border.all(
+                color: AppColors.whiteColor,
+                width: 0.0,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: _createEmptyContainer(),
+        ),
+        Expanded(
+          flex: 14,
+          child: index == itineraryResponse.steps.length + 1 ?
+          _createEmptyContainer() :
+          Container(
+              height: MediaQuery.of(context).size.height * 0.15,
+              alignment: Alignment.centerLeft,
+              child: CustomText(
+                text: "_h__",
+              )
+          ),
+        )
+      ],
+    );
+  }
+
+  ListView _getListView() {
+    return ListView.builder(
+      itemCount: itineraryResponse.steps.length + 2,
+      itemBuilder: (context, index) {
+        return Column(
+            children: [
+              if (index == 0) ... _getListHeader(context),
+              _getDotRow(context, index),
+              _getLineRow(context, index)
+            ]
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Wrapper(
         child: Container(
           padding: EdgeInsets.all(30.0),
-          child: ListView.builder(
-            itemCount: itineraryResponse.steps.length + 2,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  if (index == 0) ... [
-                    AppBar(
-                      backgroundColor: AppColors.backgroundColor,
-                      elevation: 0,
-                      title: CustomText(
-                        text: "Retour",
-                        fontSize: 22.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      leading: IconButton(
-                        icon: Icon(Icons.arrow_back),
-                        iconSize: 40.0,
-                        onPressed: () {
-                          NavigationService.backToVoiceRecognitionPage();
-                        },
-                      )
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Divider(
-                            color: AppColors.whiteColor,
-                            thickness: 1,
-                            height: 40,
-                          ),
-                          ItineraryComponent(
-                            prependText: "Départ : ",
-                            text: " ${itineraryResponse.departure}",
-                          ),
-                          SizedBox(height: 15.0),
-                          ItineraryComponent(
-                            prependText: "Arrivée : ",
-                            text: " ${itineraryResponse.destination}",
-                          ),
-                          Divider(
-                            color: AppColors.whiteColor,
-                            thickness: 1,
-                            height: 40,
-                          ),
-                          ItineraryComponent(
-                            prependText: "Durée totale : ",
-                            text: "_h__",
-                          ),
-                          SizedBox(height: 30.0),
-                          CustomText(
-                            text: "Résume de votre voyage",
-                            fontSize: 22.0,
-                          ),
-                          SizedBox(height: 30.0)
-                        ],
-                      ),
-                    ),
-                  ],
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: ItineraryComponentDot(
-                          type: index == 0
-                              ? ItineraryComponentDotType.departure
-                              : index == itineraryResponse.steps.length + 1
-                                  ? ItineraryComponentDotType.destination
-                                  : ItineraryComponentDotType.step,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: _createEmptyContainer(),
-                      ),
-                      Expanded(
-                        flex: 14,
-                        child: ItineraryComponent(
-                          text: "${_getCurrentStep(index)}",
-                        ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: index == itineraryResponse.steps.length + 1 ?
-                        _createEmptyContainer() :
-                        Container(
-                          height: MediaQuery.of(context).size.height * 0.15,
-                          decoration: BoxDecoration(
-                            color: AppColors.whiteColor,
-                            border: Border.all(
-                              color: AppColors.whiteColor,
-                              width: 0.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: _createEmptyContainer(),
-                      ),
-                      Expanded(
-                        flex: 14,
-                        child: index == itineraryResponse.steps.length + 1 ?
-                        _createEmptyContainer() :
-                        Container(
-                            height: MediaQuery.of(context).size.height * 0.15,
-                            alignment: Alignment.centerLeft,
-                            child: CustomText(
-                              text: "_h__",
-                            )
-                        ),
-                      )
-                    ],
-                  )
-                ]
-              );
-            },
-          ),
+          child: _getListView(),
         )
       )
     );
