c706b5631d6542e1d3dafd92935d96c60145d853
Clavier Eliott <clavier.eliott.el@gmail.com>
2024-01-07 11:41:43 +0100

    [AIA-TEXT-TOKEN] Fixed dataset_generator which was creating duplicates and optimized it, retrain token classification and add bag of words notebooks on 2 classifiers

--- ai/data/dataset_generator.py
+++ ai/data/dataset_generator.py
@@ -1,5 +1,6 @@
 import os
 import re
+import random
 
 import pandas as pd
 import numpy as np
@@ -8,6 +9,9 @@ import numpy as np
 class DatasetGenerator:
 
     cities = []
+    departures = []
+    arrivals = []
+
     names = []
 
     correct_sentences = []
@@ -34,11 +38,24 @@ class DatasetGenerator:
 
     fr_prefixes = [prefix for prefix, _ in prefixes]
 
+    cases = ["lower", "title", "upper"]
+    special_chars = ["-", "/"]
+
     ner_labels = ["O", "B-DEP", "I-DEP", "B-ARR", "I-ARR"]
 
     def __init__(self):
         self.load_cities()
         print(len(self.cities), "cities loaded.")
+
+        np.random.shuffle(self.cities)
+        self.departures = self.cities[:int(len(self.cities) / 2)]
+        self.arrivals = self.cities[int(len(self.cities) / 2):]
+
+        if len(self.departures) != len(self.arrivals):
+            raise Exception("Departures and arrivals are not the same length.")
+
+        print(len(self.departures), "departures loaded.")
+        print(len(self.arrivals), "arrivals loaded.")
 
         self.load_french_national_names()
         print(len(self.names), "names loaded.")
@@ -98,22 +115,36 @@ class DatasetGenerator:
 
         # keep only where Language is 0 (French)
         df = df[df["Language"] == 0]
-        self.random_sentences_french = ((df[["Text", "Language"]]
-                                  .rename(columns={"Text": "text", "Language": "NOT_FRENCH"}))
-                                 .assign(NOT_TRIP=1, UNKNOWN=0, CORRECT=0).to_dict(orient="records"))
+        self.random_sentences_french = ((df[["Text"]].rename(columns={"Text": "text"})).to_dict(orient="records"))
 
     @staticmethod
     def create_object_text_label(sentence: str, departure: str, arrival: str, prefix: str, name: str, correct: int, not_french: int, not_trip: int, unknown: int) -> dict:
+
+        # Case for unclassified sentences with two times the same city
+        if "{dup_departure}" in sentence:
+            sentence = sentence.replace("{dup_departure}", "{arrival}")
+            arrival = departure
+        elif "{dup_arrival}" in sentence:
+            sentence = sentence.replace("{dup_arrival}", "{departure}")
+            departure = arrival
+
+        # If no arrival, departure will always take prefix
+        if not arrival:
+            option = 0
+        # If no departure, arrival will always take prefix
+        elif not departure:
+            option = 1
+        # Else, randomize who takes prefix, one or both
+        else:
+            option = np.random.randint(0, 3)
+
+        departure_prefix = f"{prefix}{departure}"
+        arrival_prefix = f"{prefix}{arrival}"
+
         f_dict = {
-            "departure": np.random.choice((
-                departure,
-                f"{prefix}{departure}"
-            )),
-            "arrival": np.random.choice((
-                arrival,
-                f"{prefix}{arrival}"
-            )),
-            "name": name,
+            "departure": departure_prefix if option != 1 else departure,
+            "arrival": arrival_prefix if option != 0 else arrival,
+            "name": name
         }
 
         return {
@@ -124,14 +155,15 @@ class DatasetGenerator:
             "UNKNOWN": unknown
         }
 
-
     def fill_text_classification_sentences_templates(self, departure: str, arrival: str) -> list:
 
         correct_sentences = []
         wrong_sentences = []
         unclassified_sentences = []
 
-        for prefix in self.prefixes + [("", "")]:
+        prefixes = [random.choice(self.prefixes)] + [("", "")]
+
+        for prefix in prefixes:
 
             correct_sentences.extend([
                 self.create_object_text_label(
@@ -159,7 +191,7 @@ class DatasetGenerator:
 
             wrong_sentences.extend([
                 self.create_object_text_label(
-                    sentence, departure, arrival, prefix[0], "",
+                    sentence, departure, "", prefix[0], "",
                     0, 0, 1, 0
                 )
                 for sentence in self.wrong_sentences["only_departure_fr"]
@@ -167,7 +199,7 @@ class DatasetGenerator:
 
             wrong_sentences.extend([
                 self.create_object_text_label(
-                    sentence, departure, arrival, prefix[0], "",
+                    sentence, "", arrival, prefix[0], "",
                     0, 0, 1, 0
                 )
                 for sentence in self.wrong_sentences["only_arrival_fr"]
@@ -183,7 +215,7 @@ class DatasetGenerator:
 
             wrong_sentences.extend([
                 self.create_object_text_label(
-                    sentence, departure, arrival, prefix[1], "",
+                    sentence, departure, "", prefix[1], "",
                     0, 1, 1, 0
                 )
                 for sentence in self.wrong_sentences["only_departure_en"]
@@ -191,17 +223,19 @@ class DatasetGenerator:
 
             wrong_sentences.extend([
                 self.create_object_text_label(
-                    sentence, departure, arrival, prefix[1], "",
+                    sentence, "", arrival, prefix[1], "",
                     0, 1, 1, 0
                 )
                 for sentence in self.wrong_sentences["only_arrival_en"]
             ])
 
-        return (
-            correct_sentences +
-            unclassified_sentences +
-            np.random.choice(wrong_sentences, len(correct_sentences) - len(unclassified_sentences)).tolist()
-        )
+        sentences = []
+        sentences.extend(unclassified_sentences)
+        sentences.extend(wrong_sentences)
+
+        np.random.shuffle(correct_sentences)
+        sentences.extend(correct_sentences[:int(len(sentences))])
+        return sentences
 
     @staticmethod
     def generate_text_classification_unknown_sentences(number: int) -> list:
@@ -209,7 +243,7 @@ class DatasetGenerator:
 
         sentences = []
 
-        for _ in range(number):
+        for _ in range(int(number / 2)):
             sentences.append("".join([chr(np.random.randint(32, 127)) for _ in range(length)]))
             sentences.append("".join(np.random.choice(list("abcdefghijklmnopqrstuvwxyz      "), length)))
 
@@ -227,16 +261,18 @@ class DatasetGenerator:
     def get_text_classification_batch_sentences(self, departure: str, arrival: str) -> list:
         data = []
 
-        data.extend(self.fill_text_classification_sentences_templates(departure.title(), arrival.title()))
-        data.extend(self.fill_text_classification_sentences_templates(departure.lower(), arrival.lower()))
+        for case in self.cases:
+            for special_char in [""] + self.special_chars:
+                m_departure = getattr(departure, case)()
+                m_arrival = getattr(arrival, case)()
 
-        length = int(len(data) / 4)
+                if special_char and special_char in m_departure:
+                    m_departure = m_departure.replace(special_char, " ")
+                if special_char and special_char in m_arrival:
+                    m_arrival = m_arrival.replace(special_char, " ")
 
-        # Get as much random sentences as there are sentences in the dataset
-        data.extend(np.random.choice(self.random_sentences, length))
+                data.extend(self.fill_text_classification_sentences_templates(m_departure, m_arrival))
 
-        # Generate unknown sentences
-        data.extend(self.generate_text_classification_unknown_sentences(length))
         return data
 
     def flatten_list(self, nested_list):
@@ -326,15 +362,14 @@ class DatasetGenerator:
 
         return data
 
-    def generate_ner_tags_from_random_sentence(self) -> list:
-        sentence = np.random.choice(self.random_sentences_french)["text"]
-        formatted_sentence, _ = self.format_sentence_token_classification(sentence)
-        tags_sentence = [self.ner_labels.index("O")] * (len(formatted_sentence))
-        return [{
-            "text": sentence,
-            "tokens": formatted_sentence,
-            "ner_tags": tags_sentence,
-        }]
+    def generate_ner_tags_from_random_sentences(self, random_sentences: list) -> list:
+        for random_sentence in random_sentences:
+            sentence = random_sentence["text"]
+            formatted_sentence, _ = self.format_sentence_token_classification(sentence)
+            tags_sentence = [self.ner_labels.index("O")] * (len(formatted_sentence))
+            random_sentence["tokens"] = formatted_sentence
+            random_sentence["ner_tags"] = tags_sentence
+        return random_sentences
 
     def generate_text_classification_dataset(self, regenerate=False):
         path = "text_classification/"
@@ -349,25 +384,42 @@ class DatasetGenerator:
                 os.remove(os.path.join(path, file))
 
         dataset = []
+        arrivals = self.arrivals.copy()
 
-        for departure in self.cities:
-            arrival = np.random.choice(self.cities)
+        for departure in self.departures:
+            # pick random arrival and remove it from list
+            arrival = np.random.choice(arrivals)
+            arrivals.remove(arrival)
             dataset.extend(self.get_text_classification_batch_sentences(departure, arrival))
 
-            for special_char in ["-"]:
-                if special_char in departure:
-                    dataset.extend(self.get_text_classification_batch_sentences(departure.replace(special_char, " "), arrival))
-
+        # Create dataframe
         df = pd.DataFrame(dataset)
+
+        # Delete duplicates on text column
+        df = df.drop_duplicates(subset=["text"])
+
+        # get number of rows where CORRECT is 1
+        length = len(df[df["CORRECT"] == 1])
+
+        # Add random sentences to dataset
+        np.random.shuffle(self.random_sentences)
+        random_sentences = self.random_sentences[:int(length / 2)]
+
+        # Add unknown sentences in the dataset
+        unknown_sentences = self.generate_text_classification_unknown_sentences(length)
+        df = pd.concat([df, pd.DataFrame(random_sentences), pd.DataFrame(unknown_sentences)])
+
+        # Delete duplicates again on text column
+        df = df.drop_duplicates(subset=["text"])
 
         # Shuffle dataset since random sentences at the end weight more in file size
         df = df.sample(frac=1).reset_index(drop=True)
 
-        number_files = 4
+        number_files = 5
         for id, df_i in enumerate(np.array_split(df, number_files)):
             df_i.to_csv(f"{path}dataset_text_classification_{id + 1}.csv", index=False, sep=";")
 
-        print(f"Dataset generated with {len(dataset)} sentences.")
+        print(f"Dataset generated with {len(df)} sentences.")
 
     def generate_token_classification_dataset(self, regenerate=False):
         path = "token_classification/"
@@ -386,30 +438,35 @@ class DatasetGenerator:
         # prepare templates from correct sentences to generate strings for token classification
         sentences = self.prepare_sentences_token_classification()
 
-        for departure in self.cities:
-            steps = {"departure": departure, "arrival": np.random.choice(self.cities)}
+        arrivals = self.arrivals.copy()
 
-            for k, step in steps.items():
-                steps[k] = step.title()
-            dataset.extend(self.generate_ner_tags_from_correct_sentences(steps, sentences))
+        for departure in self.departures:
+            arrival = np.random.choice(arrivals)
+            arrivals.remove(arrival)
+            steps = {"departure": departure, "arrival": arrival}
 
-            for k, step in steps.items():
-                steps[k] = step.lower()
-            dataset.extend(self.generate_ner_tags_from_correct_sentences(steps, sentences))
+            for special_char in [""] + self.special_chars:
+                for k, step in steps.items():
+                    steps[k] = getattr(step, np.random.choice(self.cases))()
 
-            for k, step in steps.items():
-                steps[k] = step.replace("-", " ")
-            dataset.extend(self.generate_ner_tags_from_correct_sentences(steps, sentences))
+                    if special_char and special_char in step:
+                        steps[k] = step.replace(special_char, " ")
 
-        for _ in range(len(dataset)):
-            dataset.extend(self.generate_ner_tags_from_random_sentence())
+                    dataset.extend(self.generate_ner_tags_from_correct_sentences(steps, sentences))
+
+        # Add random sentences to dataset
+        random_sentences = self.generate_ner_tags_from_random_sentences(self.random_sentences_french)
+        dataset.extend(random_sentences)
 
         df = pd.DataFrame(dataset)
+
+        # Drop duplicates on text column
+        df = df.drop_duplicates(subset=["text"])
 
         # Shuffle dataset since random sentences at the end weight more in file size
         df = df.sample(frac=1).reset_index(drop=True)
 
-        number_files = 4
+        number_files = 5
         for id, df_i in enumerate(np.array_split(df, number_files)):
             df_i.to_csv(f"{path}dataset_token_classification_{id + 1}.csv", index=False, sep=";")
 
@@ -418,5 +475,5 @@ class DatasetGenerator:
 
 if __name__ == "__main__":
     generator = DatasetGenerator()
-    #generator.generate_text_classification_dataset(True)
+    generator.generate_text_classification_dataset(True)
     generator.generate_token_classification_dataset(True)
