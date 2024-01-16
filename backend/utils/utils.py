0bf72ac1f0386ad0a2c7c0552c8d297e1f44bbec
Eliott Clavier <clavier.eliott.el@gmail.com>
2024-01-16 21:18:43 +0100

    [AIA-API] If city is not in graph, search if it was not confused with station name and return the true city name

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -1,2 +1,11 @@
+import re
+
+
 def split_request_data(request_data):
-    return [sentence for sentence in request_data.decode("utf-8").splitlines() if sentence.strip()]
+    results = []
+    for sentence in request_data.decode("utf-8").splitlines():
+        if sentence.strip():
+            expr = re.compile(r'(\d+),(.*)')
+            match = expr.match(sentence)
+            results.append(f"0,{sentence}" if not match else sentence)
+    return results
